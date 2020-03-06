c46b6e88bf1f4feb9cf58bf0b2be161862392e6e
Patrick Frank <patr.frank@gmx.de>
2020-03-06 12:59:33 +0100

    Style: entry_node.js and entry_browser.js

--- src/js/entry_browser.js
+++ src/js/entry_browser.js
@@ -1,11 +1,13 @@
+// Assets
 require('regenerator-runtime/runtime'); // babel dependency needed to compile
-
 require('../css/main.scss');
 
+// Stockfish to be serve as instances of this factory
 global.StockfishFactory = Stockfish;
 
-// eslint-disable-next-line global-require
+// Engine functions shared between browser and node implementation
 const engine = require('./engine_functions.js');
 
+// Temporary
 engine.test();
 engine.test();
--- src/js/entry_node.js
+++ src/js/entry_node.js
@@ -1,10 +1,14 @@
+// Assets
 require('regenerator-runtime/runtime'); // babel dependency needed to compile
 
-// eslint-disable-next-line global-require
+// Stockfish to be serve as instances of this factory
 global.StockfishFactory = require('stockfish.wasm');
 
-// eslint-disable-next-line global-require
+// Engine functions shared between browser and node implementation
 const engine = require('./engine_functions');
 
+// Temporary
 engine.test();
 engine.test();
+
+// module.exports go here
